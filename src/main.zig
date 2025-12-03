3786b7352ac6bb83bf4c917453bd10696cb7c10e
sch0rschi <getschii@gmail.com>
2025-12-03 18:44:45 +0100

    inline for for main() function calls

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,18 @@
-const Day1 = @import("Day1.zig");
-const Day2 = @import("Day2.zig");
-const Day3 = @import("Day3.zig");
+const std = @import("std");
+const print = std.debug.print;
+
+const Days = struct {
+    pub const Day_1 = @import("Day1.zig");
+    pub const Day_2 = @import("Day2.zig");
+    pub const Day_3 = @import("Day3.zig");
+};
 
 pub fn main() !void {
-    Day1.main();
-    Day2.main();
-    Day3.main();
+    const info = @typeInfo(Days);
+
+    inline for (info.@"struct".decls, 1..) |decl, day_index| {
+        const module = @field(Days, decl.name);
+        print("=== Starting day {} ===\n", .{day_index});
+        module.main();
+    }
 }
