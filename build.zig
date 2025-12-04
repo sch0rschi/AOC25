de1722fa1b94d24eeec98c0008dc098250bedcf8
sch0rschi <getschii@gmail.com>
2025-12-04 02:24:48 +0100

    add debug step to build.zig

--- build.zig
+++ build.zig
@@ -26,4 +26,25 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
+
+    const debug_step = b.step("debug", "Run with debug optimization");
+    const debug_exe = b.addExecutable(.{
+        .name = "AOC25-debug",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = .Debug,
+        }),
+    });
+    debug_exe.linkLibC();
+    debug_exe.linkSystemLibrary("pcre2-8");
+    b.installArtifact(debug_exe);
+
+    const debug_run = b.addRunArtifact(debug_exe);
+    debug_run.step.dependOn(b.getInstallStep());
+    debug_step.dependOn(&debug_run.step);
+
+    if (b.args) |args| {
+        debug_run.addArgs(args);
+    }
 }
