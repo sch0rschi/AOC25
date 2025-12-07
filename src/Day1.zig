de9cf4eda22a407a03f6de8380e9f103787e0443
sch0rschi <getschii@gmail.com>
2025-12-07 19:22:39 +0100

    optimize Day 1

--- src/Day1.zig
+++ src/Day1.zig
@@ -2,39 +2,44 @@ const std = @import("std");
 const print = std.debug.print;
 const tokenizeScalar = std.mem.tokenizeScalar;
 
-const Dial = @import("Dial.zig").Dial;
-const DialZeroVisitCounter = @import("Dial.zig").DialZeroVisitCounter;
-const Rotation = Dial.Rotation;
-
 const input: []const u8 = @embedFile("day1");
+const mod_value: i16 = 100;
 
 pub fn main() void {
-    solve_1();
-    solve_2();
-}
-
-fn solve_1() void {
-    var rotations = tokenizeScalar(u8, input, '\n');
-    var dial: Dial = Dial{};
+    var dial: i16 = 50;
     var zero_halt_counter: u32 = 0;
-    while (rotations.next()) |rotation_string| {
-        const rotation = Rotation.parse_rotation(rotation_string);
-        dial.apply_rotation(rotation);
-        if (dial.value == 0) {
-            zero_halt_counter += 1;
+    var zero_visit_counter: i32 = 0;
+
+    var i: usize = 0;
+    const len = input.len;
+
+    while (i < len - 1) {
+        const direction_is_left = input[i] == 'L';
+        i += 1;
+
+        var rotation_value: i16 = 0;
+        while (input[i] != '\n') : (i += 1) {
+            rotation_value *= 10;
+            rotation_value += (input[i] - '0');
         }
+        i+=1;
+
+        const full_rotations = @divTrunc(rotation_value, mod_value);
+        const remainder = @rem(rotation_value, mod_value);
+        zero_visit_counter += full_rotations;
+
+        if (direction_is_left) {
+            const delta = mod_value * @intFromBool(dial == 0) + dial;
+            dial = dial - remainder;
+            zero_visit_counter += @intFromBool(remainder >= delta);
+        } else {
+            const delta = mod_value - dial;
+            dial = dial + remainder;
+            zero_visit_counter += @intFromBool(remainder >= delta);
+        }
+        dial = @mod(dial, mod_value);
+        zero_halt_counter += @intFromBool(dial == 0);
     }
     print("{}\n", .{zero_halt_counter});
-}
-
-fn solve_2() void {
-    var rotations = tokenizeScalar(u8, input, '\n');
-    var dial = Dial{};
-    var zero_visit_counter: i32 = 0;
-    while (rotations.next()) |rotation_string| {
-        const rotation = Rotation.parse_rotation(rotation_string);
-        zero_visit_counter += dial.calculate_zero_visits(rotation);
-        dial.apply_rotation(rotation);
-    }
     print("{}\n", .{zero_visit_counter});
 }
