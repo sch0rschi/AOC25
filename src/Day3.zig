7653bda8603c06f278f6190f5f00ccbffec7ee33
sch0rschi <getschii@gmail.com>
2025-12-08 00:42:19 +0100

    optimize Day 3

--- src/Day3.zig
+++ src/Day3.zig
@@ -5,45 +5,45 @@ const tokenizeScalar = std.mem.tokenizeScalar;
 const input: []const u8 = @embedFile("day3");
 
 pub fn main() void {
-    solve_1();
-    solve_2();
-}
-
-fn solve_1() void {
-    const sum = solve(2);
-    print("{}\n", .{sum});
-}
-
-fn solve_2() void {
-    const sum = solve(12);
-    print("{}\n", .{sum});
-}
-
-fn solve(number_of_digits: usize) i64 {
-    var banks = tokenizeScalar(u8, input, '\n');
-    var sum: i64 = 0;
-    while (banks.next()) |bank| sum += calculate_max_digit_selection(bank[0..], number_of_digits);
-    return sum;
-}
-
-inline fn calculate_max_digit_selection(bank: []const u8, number_of_digits: usize) i64 {
-    var digits_buf: [100]u8 = undefined;
-    for (bank, 0..) |char, i| {
-        digits_buf[i] = std.fmt.charToDigit(char, 10) catch unreachable;
+    var sum_part_1: i64 = 0;
+    var sum_part_2: i64 = 0;
+    for (0..200) |row| {
+        const offset = 101 * row;
+        sum_part_1 += calculate_max_2_digit_selection(input[offset..offset+100]);
+        sum_part_2 += calculate_max_12_digit_selection(input[offset..offset+100]);
     }
-    const digits = digits_buf[0..bank.len];
+    print("{}\n", .{sum_part_1});
+    print("{}\n", .{sum_part_2});
+}
 
+inline fn calculate_max_2_digit_selection(bank: []const u8) i64 {
     var aggregate: i64 = 0;
     var last_max_index: usize = 0;
 
-    for (0..number_of_digits) |position| {
-        const search_slice = digits[last_max_index .. digits.len - number_of_digits + 1 + position];
+    inline for (0..2) |position| {
+        const search_slice = bank[last_max_index .. 100 - 2 + 1 + position];
         const max_value_index = std.mem.indexOfMax(u8, search_slice);
         const max_value = search_slice[max_value_index];
 
         last_max_index += max_value_index + 1;
         aggregate *= 10;
-        aggregate += max_value;
+        aggregate += max_value - '0';
+    }
+    return aggregate;
+}
+
+inline fn calculate_max_12_digit_selection(bank: []const u8) i64 {
+    var aggregate: i64 = 0;
+    var last_max_index: usize = 0;
+
+    inline for (0..12) |position| {
+        const search_slice = bank[last_max_index .. 100 - 12 + 1 + position];
+        const max_value_index = std.mem.indexOfMax(u8, search_slice);
+        const max_value = search_slice[max_value_index];
+
+        last_max_index += max_value_index + 1;
+        aggregate *= 10;
+        aggregate += max_value - '0';
     }
     return aggregate;
 }
