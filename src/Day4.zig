994d3eda781481620e9e81cf752182c9118001ed
sch0rschi <getschii@gmail.com>
2025-12-07 23:14:03 +0100

    optimize Day 4

--- src/Day4.zig
+++ src/Day4.zig
@@ -3,124 +3,101 @@ const print = std.debug.print;
 
 const input: []const u8 = @embedFile("day4");
 
-const MAX_SIZE = 137 + 2;
-
-const Grid = struct {
-    data: [MAX_SIZE * MAX_SIZE]bool,
-    width: usize,
-    height: usize,
-
-    fn init() Grid {
-        var grid = Grid{
-            .data = undefined,
-            .width = 0,
-            .height = 0,
-        };
-
-        var lines = std.mem.tokenizeScalar(u8, input, '\n');
-        var row: usize = 1;
-        grid.width = lines.peek().?.len + 2;
-        while (lines.next()) |line| : (row += 1) {
-            for (line, 1..) |char, col| {
-                grid.set(row, col, char == '@');
-            }
-        }
-        grid.height = row + 1;
-        for (0..grid.width) |column_index| {
-            grid.set(0, column_index, false);
-            grid.set(grid.height - 1, column_index, false);
-        }
-        for (0..grid.height) |row_index| {
-            grid.set(row_index, 0, false);
-            grid.set(row_index, grid.width - 1, false);
-        }
-
-        return grid;
-    }
-
-    inline fn get(self: *const Grid, row: usize, col: usize) bool {
-        return self.data[row * MAX_SIZE + col];
-    }
-
-    inline fn set(self: *Grid, row: usize, col: usize, value: bool) void {
-        self.data[row * MAX_SIZE + col] = value;
-    }
-};
+const SIZE_WITH_BORDER: usize = 137 + 2;
+const GRID_AREA = SIZE_WITH_BORDER * SIZE_WITH_BORDER;
+const SIZE: usize = 137;
 
 pub fn main() void {
-    var grid = Grid.init();
+    var data: [GRID_AREA]bool = undefined;
+    var data_counter: [GRID_AREA]i8 = undefined;
+    for (0..SIZE_WITH_BORDER) |column_index| {
+        data[column_index] = false;
+        data[GRID_AREA - SIZE_WITH_BORDER + column_index] = false;
+        data_counter[column_index] = -1;
+        data_counter[GRID_AREA - SIZE_WITH_BORDER + column_index] = -1;
+    }
+    for (0..SIZE_WITH_BORDER) |row_index| {
+        data[row_index * SIZE_WITH_BORDER] = false;
+        data[row_index * SIZE_WITH_BORDER + SIZE_WITH_BORDER - 1] = false;
+        data_counter[row_index * SIZE_WITH_BORDER] = -1;
+        data_counter[row_index * SIZE_WITH_BORDER + SIZE_WITH_BORDER - 1] = -1;
+    }
 
-    solve_1(&grid);
-    solve_2(&grid);
-}
+    var input_index: usize = 0;
+    for (1..SIZE + 1) |row_index| {
+        const offset = row_index * SIZE_WITH_BORDER;
+        for (1..SIZE + 1) |col_index| {
+            data[offset + col_index] = input[input_index] == '@';
+            input_index += 1;
+        }
+        input_index += 1;
+    }
 
-fn solve_1(grid: *const Grid) void {
-    const sum = count_removals(grid);
+    var sum: i16 = 0;
+    for (1..SIZE + 1) |row| {
+        const offset = row * SIZE_WITH_BORDER;
+        for (1..SIZE + 1) |col| {
+            if (!data[offset + col]) {
+                data_counter[offset + col] = -1;
+                continue;
+            }
+
+            const counter: i8 = count_neighbours(&data, offset + col);
+            data_counter[offset + col] = @max(1, counter);
+            if (counter < 4) {
+                sum += 1;
+            }
+        }
+    }
+    print("{}\n", .{sum});
+
+    sum = 0;
+    for (1..SIZE + 1) |row| {
+        const offset = row * SIZE_WITH_BORDER;
+        for (1..SIZE + 1) |col| {
+            if (data_counter[offset + col] > 0 and data_counter[offset + col] < 4) {
+                sum += count_removables_recursion(&data_counter, offset + col);
+            }
+        }
+    }
+
     print("{}\n", .{sum});
 }
 
-inline fn count_removals(grid: *const Grid) i32 {
-    var sum: i32 = 0;
-
-    for (1..grid.height - 1) |row| {
-        for (1..grid.width - 1) |col| {
-            if (!grid.get(row, col)) continue;
-
-            const counter: u8 = count_neighbours(grid, row, col);
-
-            if (counter < 4) sum += 1;
-        }
-    }
-
-    return sum;
-}
-
-inline fn count_neighbours(grid: *const Grid, row: usize, col: usize) u8 {
+inline fn count_neighbours(data: *[GRID_AREA]bool, offset: usize) i8 {
     return
-        @as(u8, @intFromBool(grid.get(row - 1, col - 1))) +
-        @as(u8, @intFromBool(grid.get(row - 1, col))) +
-        @as(u8, @intFromBool(grid.get(row - 1, col + 1))) +
-        @as(u8, @intFromBool(grid.get(row, col - 1))) +
-        @as(u8, @intFromBool(grid.get(row, col + 1))) +
-        @as(u8, @intFromBool(grid.get(row + 1, col - 1))) +
-        @as(u8, @intFromBool(grid.get(row + 1, col))) +
-        @as(u8, @intFromBool(grid.get(row + 1, col + 1)));
+        @as(i8, @intFromBool(data[offset - SIZE_WITH_BORDER - 1])) +
+        @as(i8, @intFromBool(data[offset - SIZE_WITH_BORDER])) +
+        @as(i8, @intFromBool(data[offset - SIZE_WITH_BORDER + 1])) +
+        @as(i8, @intFromBool(data[offset - 1])) +
+        @as(i8, @intFromBool(data[offset + 1])) +
+        @as(i8, @intFromBool(data[offset + SIZE_WITH_BORDER - 1])) +
+        @as(i8, @intFromBool(data[offset + SIZE_WITH_BORDER])) +
+        @as(i8, @intFromBool(data[offset + SIZE_WITH_BORDER + 1]));
 }
 
-fn solve_2(grid: *Grid) void {
-    print("{}\n", .{count_removables(grid)});
-}
-
-inline fn count_removables(grid: *Grid) i16 {
+fn count_removables_recursion(grid: *[GRID_AREA]i8, offset: usize) i16 {
     var sum: i16 = 0;
-    for (1..grid.height - 1) |row| {
-        for (1..grid.width - 1) |col| {
-            if (grid.get(row, col)) {
-                sum += count_removables_recursion(grid, row, col);
-            }
-        }
-    }
-
-    return sum;
-}
-
-fn count_removables_recursion(grid: *Grid, row: usize, col: usize) i16 {
-    if (!grid.get(row, col)) {
-        return 0;
-    }
-    var sum: i16 = 0;
-    const removed: u8 = count_neighbours(grid, row, col);
-    if (removed < 4) {
+    if (grid[offset] >= 0 and grid[offset] < 4) {
         sum += 1;
-        grid.set(row, col, false);
-        sum += count_removables_recursion(grid, row + 1, col);
-        sum += count_removables_recursion(grid, row + 1, col + 1);
-        sum += count_removables_recursion(grid, row, col + 1);
-        sum += count_removables_recursion(grid, row - 1, col + 1);
-        sum += count_removables_recursion(grid, row - 1, col);
-        sum += count_removables_recursion(grid, row - 1, col - 1);
-        sum += count_removables_recursion(grid, row, col - 1);
-        sum += count_removables_recursion(grid, row + 1, col - 1);
+        grid[offset] = -1;
+
+        grid[offset - SIZE_WITH_BORDER - 1] -= 1;
+        grid[offset - SIZE_WITH_BORDER] -= 1;
+        grid[offset - SIZE_WITH_BORDER + 1] -= 1;
+        grid[offset - 1] -= 1;
+        grid[offset + 1] -= 1;
+        grid[offset + SIZE_WITH_BORDER - 1] -= 1;
+        grid[offset + SIZE_WITH_BORDER] -= 1;
+        grid[offset + SIZE_WITH_BORDER + 1] -= 1;
+        sum += count_removables_recursion(grid, offset - SIZE_WITH_BORDER - 1);
+        sum += count_removables_recursion(grid, offset - SIZE_WITH_BORDER);
+        sum += count_removables_recursion(grid, offset - SIZE_WITH_BORDER + 1);
+        sum += count_removables_recursion(grid, offset - 1);
+        sum += count_removables_recursion(grid, offset + 1);
+        sum += count_removables_recursion(grid, offset + SIZE_WITH_BORDER - 1);
+        sum += count_removables_recursion(grid, offset + SIZE_WITH_BORDER);
+        sum += count_removables_recursion(grid, offset + SIZE_WITH_BORDER + 1);
     }
     return sum;
 }
