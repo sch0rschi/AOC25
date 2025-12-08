e8a6ca65326b83d55e4450806d582c8a53ffd5fa
sch0rschi <getschii@gmail.com>
2025-12-08 03:14:38 +0100

    optimize Day 7

--- src/Day7.zig
+++ src/Day7.zig
@@ -1,54 +1,38 @@
 const std = @import("std");
 const print = std.debug.print;
-const tokenizeScalar = std.mem.tokenizeScalar;
 
 const input: []const u8 = @embedFile("day7");
 
 pub fn main() void {
-    var line_tokens = tokenizeScalar(u8, input, '\n');
-    var grid: [71][141]i64 = undefined;
-    var row_index_counter: usize = 0;
-    var width: usize = 0;
+    var row: [141]u64 = undefined;
+    var count: u16 = 0;
 
-    while (line_tokens.next()) |line_string| : (row_index_counter += 1) {
-        for (line_string, 0..) |character, column_index| {
-            switch (character) {
-                '.' => grid[row_index_counter][column_index] = 0,
-                '^' => grid[row_index_counter][column_index] = -1,
-                else => grid[row_index_counter][column_index] = 1,
-            }
-            width = column_index+1;
-        }
-        _ = line_tokens.next();
+    for (0..141) |col| {
+        row[col] = @intFromBool(input[col] != '.');
     }
-    var grid_slice = grid[0..row_index_counter];
 
-    var count: i32 = 0;
-    for(grid_slice[1..], 1..) |row, row_index| {
-        for (row[0..width], 0..) |cell, column_index| {
-            switch (cell) {
-                -1 => {
-                    if (grid_slice[row_index - 1][column_index] >= 1) {
-                        count += 1;
-                        grid_slice[row_index][column_index-1] += grid_slice[row_index - 1][column_index];
-                        grid_slice[row_index][column_index+1] += grid_slice[row_index - 1][column_index];
-                    }
-                },
-                else => {
-                    if (grid_slice[row_index - 1][column_index] >= 1) {
-                        grid_slice[row_index][column_index] += grid_slice[row_index - 1][column_index];
-                    }
-                },
-            }
+    for (1..71) |half_row_index| {
+        const input_offset = 2 * half_row_index * 142;
+        for (71-half_row_index.. 70+half_row_index) |i| {
+            const is_caret = input[input_offset + i] == '^';
+            const grid_val = row[i];
+
+            count += @as(u16, @intFromBool(is_caret and grid_val > 0));
+
+            const val_to_add = @intFromBool(is_caret) * grid_val;
+            row[i - 1] += val_to_add;
+            row[i + 1] += val_to_add;
+            row[i] -= val_to_add;
         }
     }
 
     print("{}\n", .{count});
-    var sum: i64 = 0;
-    for (grid_slice[row_index_counter-1][0..width]) |value| {
-        if (value > 0) {
-            sum += value;
-        }
+
+    // Sum all non-zero values
+    var sum: u64 = 0;
+    for (row) |value| {
+        sum += value;
     }
+
     print("{}\n", .{sum});
 }
